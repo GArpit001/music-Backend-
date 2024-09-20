f6aa8760ce6e330f4032e27d38b8da41f1c041b7
Arpit <arpitvermaetw@gmail.com>
2024-09-20 22:56:32 +0530

    Add Cluster and Create intance of CPU

--- server.js
+++ server.js
@@ -5,42 +5,48 @@ import connectCloudinery from "./cloudinary.js";
 import { v2 as cloudinery } from "cloudinary";
 import connectDataBase from "./config/db.js";
 import cors from "cors";
-import "dotenv/config"
+import "dotenv/config";
 import router1 from "./router/songRouter.js";
 import router2 from "./router/albumRouter.js";
+import cluster from "cluster";
+import os from "os";
 
-const app = express();
-
-// SUPPORT PORT 
-const port = process.env.PORT || 4000;
-
-
-// DataBase
-connectDataBase();
-connectCloudinery();
+const CPU = os.cpus().length;
+// console.log(CPU);
 
 
-// middleware 
-app.use(express.json());
-app.use(cors());
+if (cluster.isPrimary) {
+  for (let i = 0; i < CPU; i++) {
+    cluster.fork();
+  }
+} else {
+  const app = express();
 
+  // SUPPORT PORT
+  const port = process.env.PORT || 4000;
 
+  // DataBase
+  connectDataBase();
+  connectCloudinery();
 
-//  All routes 
+  // middleware
+  app.use(express.json());
+  app.use(cors());
 
+  //  All routes
 
-// SONG ROUTES 
+  // SONG ROUTES
 
-app.use("/hello" , router1)
-app.use("/api/song" , router1)
+  app.use("/hello", router1);
+  app.use("/api/song", router1);
 
+  // ALBUM ROUTES
 
-// ALBUM ROUTES
+  app.use("/hello2", router2);
+  app.use("/api/album", router2);
 
-app.use("/hello2" , router2)
-app.use("/api/album" , router2)
-
-// CALL THE SERVER 
-app.listen(port, () => {
-  console.log("Server listening on port no. " + port);
-});
+  // CALL THE SERVER
+  app.listen(port, () => {
+    console.log("Server listening on port no. " + port);
+  });
+}
